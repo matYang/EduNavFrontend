b644a50602ec71791e13e11899298e997ae6a8bf
jzlingmo <120237546@qq.com>
2014-08-29 00:42:45 +0800

    cancel booking

--- scripts/User/View/MyPage/BookingListView.js
+++ scripts/User/View/MyPage/BookingListView.js
@@ -61,27 +61,30 @@ var BookingListView = MultiPageView3.extend({
         $("#bookingSummary").on("click", ".js_btn_operate", function (e) {
             var $target = $(e.target);
             var bookingId = $target.data('id');
+            if(!bookingId){
+                return
+            }
             var operate = $target.data('action');
-            $("#cancelBooking").val("更改中...");
+            $target.val("更改中...");
             app.userManager.changeBookingState(bookingId, operate, {
                 success: function (booking) {
                     var status;
                     if(operate === 'offlineCancel'||operate === 'onlineCancel'){
-                        $target.html("<p>已取消</p>");
+                        $target.html("已取消");
                     }else if(operate === 'offlineDelayed'){
-                        $target.html("<p>已推迟</p>");
+                        $target.html("已推迟");
                     }else{
-                        $target.html("<p>操作成功</p>");
+                        $target.html("操作成功");
                     }
-
+                    $target.data('id','');//取消id防止请求触发
                     $("#bookingStatus").html(EnumConfig.BookingStatusText[booking.status]);
                     //TODO 这里进行了推迟操作以后是否可以继续取消订单 操作部分的刷新后续需要单独提取出来模板进行render
                     $target.unbind('click');
                 },
                 error: function (data) {
-                    $target.val("操作失败，请重试");
+                    $target.html("操作失败，请重试");
                     if (data) {
-                        Info.displayNotice(data.responseJSON.message);
+                        Info.displayNotice(data.responseJSON&&data.responseJSON.message);
                     }
                 }
             });
