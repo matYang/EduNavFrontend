1f410e611839549d02dc77561717b0ae47256588
Jet <120237546@qq.com>
2014-09-16 09:37:29 +0800

    add locationId when pull school list

--- scripts/User/View/MyPage/MyPageChooseSchoolModal.js
+++ scripts/User/View/MyPage/MyPageChooseSchoolModal.js
@@ -58,7 +58,7 @@ var ChooseSchoolView = Backbone.View.extend({
         $('#chooseshoolshi').change(function () {
             var locationId = $(this).val();
             if (!locationId)return;
-            app.generalManager.fetchSchools({
+            app.generalManager.fetchSchools(locationId, {
                 success: self.renderSchools,
                 error: self.renderSchoolsError
             });
