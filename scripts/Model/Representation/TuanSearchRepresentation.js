749d6514780ba4405e0b019cee4caccf230bfa6b
Jet <120237546@qq.com>
2014-09-26 11:02:48 +0800

    tuan search Representation added

---
+++ scripts/Model/Representation/TuanSearchRepresentation.js
@@ -0,0 +1,109 @@
+var TuanearchRepresentation = Backbone.Model.extend({
+    defaults: function () {
+        //todo 确定需要过滤的字段条件
+        return {
+
+            'templateId':undefined,//相关课程搜索（同一个模板）
+
+            'categoryValue': undefined,//筛选课程类目
+            'locationValue': undefined,//筛选地址
+            'circleValue': undefined,//筛选商圈
+
+            'institutionName': undefined,
+            'status': EnumConfig.CourseStatus.onlined,
+
+            //开课和结课日期
+            'startDate': undefined,
+            'finishDate': undefined,
+            //上课的时间段
+            'schooltimeWeek': undefined,//平时 周末
+            'schooltimeDay': undefined,//上午 下午 晚上
+            //开课日期(start end)
+            'startDateStart': undefined,
+            'startDateEnd': undefined,
+            //课程费用(start end)
+            'priceStart': undefined,
+            'priceEnd': undefined,
+            'classType': undefined,//班级类型 0，1，2，3
+
+            'commission': undefined,
+            'originalPriceStart': undefined,
+            'cashbackStart': undefined,
+            'cashbackEnd': undefined,
+
+            'courseReference': undefined,
+            'partnerReference': undefined,
+
+            'courseId': undefined,
+            'partnerId': undefined,
+            'userId': undefined,
+
+            'useCache': 1,
+            'startcreateTime': undefined,
+            'finishcreateTime': undefined,
+            'startCutoffDate': undefined,
+            'finishCutoffDate': undefined,
+            'startUponArrival': undefined,
+
+            'start': undefined,
+            'count': undefined,
+
+            'order': undefined,
+            'columnKey': undefined,
+
+            'enabled':1//等于1 会将templateId相同的课程只显示一个 在相关课程中调用需要设为undefined
+        };
+    },
+
+    initialize: function () {
+        _.bindAll(this, 'toQueryString', 'castFromQuery', 'toJSON');
+    },
+
+    toQueryString: function () {
+        var queryArr = [];
+        var queryObj = this.toJSON();
+        queryObj.useCache = undefined;
+        for (var property in queryObj) {
+            if (queryObj.hasOwnProperty(property) && typeof queryObj[property] !== 'undefined') {
+                queryArr.push(property + '=' + queryObj[property]);
+            }
+        }
+
+        return queryArr.join('&');
+    },
+
+    castFromQuery: function (queryObj) {
+        var obj, i, temp;
+        if (typeof queryObj === "string") {
+            obj = queryObj.split("&");
+            queryObj = {};
+            for (i = 0; i < obj.length; i++) {
+                temp = obj[i].split("=");
+                queryObj[temp[0]] = temp[1];
+            }
+        }
+        for (var property in queryObj) {
+            if (queryObj.hasOwnProperty(property) && typeof queryObj[property] !== 'undefined') {
+                this.set(property, decodeURI(queryObj[property]));
+                if (property.indexOf("Date") > 0) {
+                    this.set(property, Utilities.castFromAPIFormat(queryObj[property]));
+                }
+            }
+        }
+    },
+
+    toJSON: function () {
+        var queryObj = _.clone(this.attributes);
+        queryObj.startDateStart = typeof this.get('startDateStart') === 'undefined' ? undefined : Utilities.castToAPIFormat(this.get('startDateStart'));
+        queryObj.startDateEnd = typeof this.get('startDateEnd') === 'undefined' ? undefined : Utilities.castToAPIFormat(this.get('startDateEnd'));
+        queryObj.createTimeStart = typeof this.get('createTimeStart') === 'undefined' ? undefined : Utilities.castToAPIFormat(this.get('createTimeStart'));
+        queryObj.createTimeEnd = typeof this.get('createTimeEnd') === 'undefined' ? undefined : Utilities.castToAPIFormat(this.get('createTimeEnd'));
+        return queryObj;
+    },
+    toTitleString: function () {
+        var buf = "";
+
+        return buf;
+    }
+
+});
--- scripts/View/TuanView.js
+++ scripts/View/TuanView.js
@@ -1,41 +1,69 @@
 var TuanView = Backbone.View.extend({
-    el: "#content",
-    //todo need to write template named 'tpl_tuan'
-    template: _.template(tpl.get("tuan")),
-    initialize: function () {
-        this.isClosed = false;
-        _.bindAll(this, "render", "bindEvents", "close");
-        app.viewRegistration.register(this);
-        this.render();
+        el: "#content",
+        //todo need to write template named 'tpl_tuan'
+        template: _.template(tpl.get("tuan")),
+        initialize: function () {
+            this.isClosed = false;
+            _.bindAll(this, "render", "bindEvents", "close");
+            app.viewRegistration.register(this);
+            //从本地初始化搜索条件
+            this.searchRepresentation = app.storage.getSearchRepresentationCache("tuan");
 
-    },
-    render: function () {
-        this.$el.html(this.template());
-        if (!this.tuanBannerView) {
-            this.tuanBannerView = new TuanBannerView();
-        } else if (this.tuanBannerView.isClosed) {
-            this.tuanBannerView.render();
-        }
-        if (!this.tuanResultView) {
-            this.tuanResultView = new TuanResultView();
-        } else if (this.tuanResultView.isClosed) {
-            this.tuanResultView.render();
-        }
-        this.bindEvents();
-    },
+            this.render();
 
-    bindEvents: function () {
-        var that = this;
-    },
+        },
+        render: function () {
+            this.$el.html(this.template());
+            if (!this.tuanBannerView) {
+                this.tuanBannerView = new TuanBannerView();
+            } else if (this.tuanBannerView.isClosed) {
+                this.tuanBannerView.render();
+            }
+            if (!this.tuanResultView) {
+                this.tuanResultView = new TuanResultView();
+            } else if (this.tuanResultView.isClosed) {
+                this.tuanResultView.render();
+            }
+            this.bindEvents();
+        },
 
-    close: function () {
-        if (!this.isClosed) {
-            this.$el.off();
-            this.$el.empty();
-            this.isClosed = true;
+        bindEvents: function () {
+            var that = this;
+            //绑定右栏筛选按钮 todo 设置searchRepresentationModel存储当前的筛选条件
+            $('.labels-category').on('click', 'span', function (e) {
+                var $target = $(e.target);
+                if ($target.hasClass('active')) {
+                    return
+                }
+                $target.siblings().removeClass('active');
+                $target.addClass('active');
+                //todo 这里添加设置search按钮 直接诶fetch
+            });
+            $('.labels-location').on('click', 'span', function (e) {
+                var $target = $(e.target);
+                if ($target.hasClass('active')) {
+                    return
+                }
+                $target.siblings().removeClass('active');
+                $target.addClass('active');
+            })
+
+        },
+        //用于团购数据的获取 todo赢
+        fetchAction: function () {
+
+        },
+
+        close: function () {
+            if (!this.isClosed) {
+                this.$el.off();
+                this.$el.empty();
+                this.isClosed = true;
+                $('.labels').off();
+            }
         }
-    }
-});
+    })
+    ;
 
 /*团BannerView*/
 var TuanBannerView = Backbone.View.extend({
@@ -56,12 +84,12 @@ var TuanBannerView = Backbone.View.extend({
 
     bindEvents: function () {
         var that = this;
-        $(".tips .tips_li").hover(function(){
-            var bindex=$(this).attr("index");
+        $(".tips .tips_li").hover(function () {
+            var bindex = $(this).attr("index");
             $(".tips .tips_li").removeClass("active");
             $(this).addClass("active");
             $(".pic .pics").removeClass("active");
-            $(".pic .pics:eq("+bindex+")").addClass("active");
+            $(".pic .pics:eq(" + bindex + ")").addClass("active");
         });
     },
 
